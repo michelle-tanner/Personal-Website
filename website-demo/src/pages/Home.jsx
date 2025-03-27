46f66a352dc8b6eea144f603e086d1f23dcb4685
Michelle <michellemt0518@gmail.com>
2025-03-27 15:19:15 -0500

    updated Favorites.jsx file to include searchQuery and EventCards instead of Home.jsx

--- website-demo/src/pages/Home.jsx
+++ website-demo/src/pages/Home.jsx
@@ -6,13 +6,6 @@ function Home() {
     const [searchQuery, setSearchQuery] = useState("");
 
 
-    const events = [
-        {id: 1, title: "St. Patricks Day", url: "", date: "3/17/2024", description: "This is the day..."},
-        {id: 2, title: "Birthday Celebration", url: "", date: "3/14/2024", description: "First day we met.."},
-        {id: 3, title: "Fogo de Chao", url: "", date: "3/22/2024", description: "Went to fogo de chao"},
-        {id: 4, title: "Lunar Eclipse", url: "", date: "3/01/2024", description: "Something else...."},    
-
-    ]
     const handleSearch = (e) => {
         e.preventDefault()
         alert(searchQuery)
@@ -37,18 +30,6 @@ function Home() {
 
         <div>Input Typed (real-time): <br></br>{searchQuery}</div>
 
-
-        <div className="events-grid">
-            {/* {events.map((event) => (
-                <EventCard event={event} key={event.id}/>
-            ))} */}
-
-            {events.map((event) => (
-                event.title.toLowerCase().startsWith(searchQuery) && (
-                    <EventCard event={event} key={DeviceMotionEvent.id} />
-                )
-            ))}
-        </div>
     </div>
     );
 }

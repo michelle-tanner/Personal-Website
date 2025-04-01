d48448f5f275d747308308622e78a26cb1a19696
Michelle <michellemt0518@gmail.com>
2025-04-01 00:11:30 -0500

    updates

--- website-demo/src/pages/Favorites.jsx
+++ website-demo/src/pages/Favorites.jsx
@@ -8,7 +8,7 @@ function Favorite() {
 
     const events = [
         {id: 1, title: "St. Patricks Day", url: "", date: "3/17/2024", description: "This is the day..."},
-        {id: 2, title: "Birthday Celebration", url: "", date: "3/14/2024", description: "First day we met.."},
+        {id: 2, title: "Birthday Celebration", url: "", date: "3/14/2024", description: "Strawberry Cake slicing.."},
         {id: 3, title: "Fogo de Chao", url: "", date: "3/22/2024", description: "Went to fogo de chao"},
         {id: 4, title: "Lunar Eclipse", url: "", date: "3/01/2024", description: "Something else...."},    
 

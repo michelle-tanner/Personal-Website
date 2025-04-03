dc76a0bfc9acd4731fcf49c6230a19a636f13670
Michelle <michellemt0518@gmail.com>
2025-04-03 00:20:59 -0500

    Add Projects Card component and initial html

--- website-demo/src/pages/Projects.jsx
+++ website-demo/src/pages/Projects.jsx
@@ -3,30 +3,20 @@ import "../css/index.css"
 function Projects() {
 
     const projects = [ // object 
-        {id: 1, title: "St. Patricks Day", url: "", date: "3/17/2024", description: "This is the day..."},
-        {id: 2, title: "Birthday Celebration", url: "", date: "3/14/2024", description: "Strawberry Cake slicing.."},
-        {id: 3, title: "Fogo de Chao", url: "", date: "3/22/2024", description: "Went to fogo de chao"},
-        {id: 4, title: "Lunar Eclipse", url: "", date: "3/01/2024", description: "Something else...."},    
-
+        {id: 1, title: "Project 1", link: "", description: "words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more"},
+        {id: 2, title: "Project 2", link: "", description: "words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption."},
+        {id: 3, title: "Project 3", link: "", description: "words words desciption and more words words desciption and more words words desciption and more words words desciption."},
     ]
 
     return (
         <>
         <div className="projects">
             <div className="description">
-                <h3>Project 1</h3>
-                <p>
-                    words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more<br></br>
-                    <br></br>
-                    Inspired by: Camille Amore or something 
-                    Check out the Project: 
-                </p>
-                <img className="image" src="" alt="" title=""></img>
             </div>
             <div className="description">
             <h3>Project 2</h3>
                 <p>
-                    words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more<br></br>
+                    words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption and more words words desciption.<br></br>
                     <br></br>
                     Inspired by: Camille Amore or something 
                     Check out the Project: 

2e58e925419536f22eab1530110a585326742af9
michelle <michellemt0518@gmail.com>
2025-05-20 16:06:10 -0500

    finished ContactMe

--- website-demo/src/components/ContactMe.jsx
+++ website-demo/src/components/ContactMe.jsx
@@ -3,18 +3,17 @@ import '../css/ContactMe.css'
 function Contact() {
     return (
         <>
-        Tech Stack
-        <div className="info">
-           <div className="linkedin">
-                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/icons/linkedin.svg" alt="img"></img>LinkedIn</a>
+        <div className="information info">
+           <div className="contact">
+                <a href="https://linkedin.com/in/michelletanner05" target="_blank"><img src="src/assets/icons/linkedin.svg" alt="img"></img>LinkedIn</a>
                 michelle-tanner-05
            </div>
-           <div className="linkedin">
-                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/icons/github.svg" alt="img"></img>Github</a>
+           <div className="contact">
+                <a href="https://github.com/michelle-tanner" target="_blank"><img src="src/assets/icons/github.svg" alt="img"></img>Github</a>
                 michelle-tanner
            </div>
-           <div className="linkedin">
-                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/icons/mail.svg" alt="img"></img>Mail Me!</a>
+           <div className="contact">
+                <a href="mailto:michellemt0518@gmail.com" target="_blank"><img src="src/assets/icons/mail.svg" alt="img"></img>Email Me!</a>
                 michellemt0518@gmail.com
            </div>
         </div>

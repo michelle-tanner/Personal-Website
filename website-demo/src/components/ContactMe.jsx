608de5c29ef1e8d63ff0f668a5b89bf0db322acb
michelle <michellemt0518@gmail.com>
2025-05-20 15:56:16 -0500

    light-mode ContactMe component

--- website-demo/src/components/ContactMe.jsx
+++ website-demo/src/components/ContactMe.jsx
@@ -6,15 +6,15 @@ function Contact() {
         Tech Stack
         <div className="info">
            <div className="linkedin">
-                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/tech_stack/linkedin.svg" alt="img"></img>LinkedIn</a>
+                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/icons/linkedin.svg" alt="img"></img>LinkedIn</a>
                 michelle-tanner-05
            </div>
-           <div className="github">
-                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/tech_stack/linkedin.svg" alt="img"></img>Github</a>
+           <div className="linkedin">
+                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/icons/github.svg" alt="img"></img>Github</a>
                 michelle-tanner
            </div>
-           <div className="email">
-                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/tech_stack/linkedin.svg" alt="img"></img>Mail Me!</a>
+           <div className="linkedin">
+                <a href="https://linkedin.com/michelle-tanner0518" target="_blank"><img src="src/assets/icons/mail.svg" alt="img"></img>Mail Me!</a>
                 michellemt0518@gmail.com
            </div>
         </div>

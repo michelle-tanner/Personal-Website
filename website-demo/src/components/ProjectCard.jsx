3e8ca9f510663f293f5a99b71b09a2a92c533658
michelle <michellemt0518@gmail.com>
2025-05-30 15:07:47 -0500

    changes: taking a mental break

--- website-demo/src/components/ProjectCard.jsx
+++ website-demo/src/components/ProjectCard.jsx
@@ -9,7 +9,7 @@ function ProjectCard(project) {
     return <div className="project-card">
         <img className="project-image" src={project.imgURL} alt="project display"></img>
         <div className="title">{project.name}</div>
-        <div className="description">description here...description here...description here...</div>
+        <div className="description">{project.description}</div>
         <div className="btns">
             <button className="demo">Live Demo</button>
             <button className="source">Source Code</button>
--- website-demo/src/pages/Projects.jsx
+++ website-demo/src/pages/Projects.jsx
@@ -8,11 +8,11 @@ function Projects() {
     return (
         <>
         <div className="projects">
-            <Project style="flex-grow: 1" name="Voyage Game" imgURL="src/assets/images/mountain.png" />
-            <Project style="flex-grow: 1" name="Cow Chase" imgURL="src/assets/images/blue_line.png" />
-            <Project style="flex-grow: 1" name="Pad Thai" imgURL="src/assets/images/dave_and_busters.png" />
-            <Project style="flex-grow: 1" name="WiCs Website" imgURL="src/assets/images/uic.png" />
-            <Project style="flex-grow: 1" name="Personal Website" imgURL="src/assets/images/cta_stop.png" />
+            <Project style="flex-grow: 1" name="Voyage Game" imgURL="src/assets/images/mountain.png" description="Used python on PyCharm IDE with Grpahic libraries and 2D pixels." stack="python, pycahrm"/>
+            <Project style="flex-grow: 1" name="Cow Chase" imgURL="src/assets/images/blue_line.png" description=""/>
+            <Project style="flex-grow: 1" name="Pad Thai" imgURL="src/assets/images/dave_and_busters.png" description=""/>
+            <Project style="flex-grow: 1" name="WiCs Website" imgURL="src/assets/images/uic.png" description=""/>
+            <Project style="flex-grow: 1" name="Personal Website" imgURL="src/assets/images/cta_stop.png" description=""/>
         </div>
 
         

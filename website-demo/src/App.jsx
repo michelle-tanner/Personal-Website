e0d4e3f35b4be3016f1e34f4c2d563e6a1a10e97
michelle <michellemt0518@gmail.com>
2025-06-03 10:06:13 -0500

    fix website view-height (vh)

--- website-demo/src/App.jsx
+++ website-demo/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <>
-    <div data-theme="dark">
+    <div className="screen" data-theme="dark">
       <Navigation />
       <main className="main-content">
         {/* <h3 className="substitute">...Main Content...</h3> */}

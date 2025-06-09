b1fcbaaa82e5ce5401892387ec85dddf5d7b226e
michelle <michellemt0518@gmail.com>
2025-06-09 14:21:24 -0500

    description of projects

--- website-demo/src/pages/Projects.jsx
+++ website-demo/src/pages/Projects.jsx
@@ -6,11 +6,11 @@ function Projects() {
     return (
         <>
         <div className="wrapper">
-            <Project num="1" name="Voyage Game" description="this is a test description for a new one." url="../assets/images/blue_line.png"/>
-            <Project num="2" name="Cow Chase" description="this is a test description for a new one." url="../assets/images/cta_stop.png"/>
-            <Project num="3" name="SparkHacks" description="this is a test description for a new one." url="../assets/images/mango_mango.png"/>
-            <Project num="4" name="Personal Website" description="this is a test description for a new one." url="../assets/images/skiing.png"/>
-            <Project num="5" name="WiCs Website" description="this is a test description for a new one." url="../assets/images/pool.png"/>
+            <Project num="1" name="Voyage Game" description="Python on PyCharm IDE for single player game" url="../assets/images/blue_line.png"/>
+            <Project num="2" name="Cow Chase" description="Java on IntelliJ IDE for multiplayer game" url="../assets/images/cta_stop.png"/>
+            <Project num="3" name="SparkHacks" description="Astro, HTML, css, for Thai Bowl small-business" url="../assets/images/mango_mango.png"/>
+            <Project num="4" name="Personal Website" description="React, Javascript, for personal website" url="../assets/images/skiing.png"/>
+            <Project num="5" name="WiCs Website" description="Astro, Typescript, Tailwind css for Women in Computer Science @ UIC" url="../assets/images/pool.png"/>
         </div>
         </>
     )

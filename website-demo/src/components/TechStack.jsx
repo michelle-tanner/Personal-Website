0083ba61a0c5429fb7550101b76ecc91f46198c5
michelle <michellemt0518@gmail.com>
2025-07-02 13:46:10 -0500

    tech stack text-color repsonsive

--- website-demo/src/components/TechStack.jsx
+++ website-demo/src/components/TechStack.jsx
@@ -7,22 +7,22 @@ function TechStack() {
         <div className="info bubble">
             <div className="horizontal">
                 <div className="fullstack">Web-Development
-                    <a href="https://www.javascript.com/" target="_blank"><img src="src/assets/tech_stack/js.svg" alt="javascript"></img>JavaScript</a>
-                    <a href="https://react.dev/" target="_blank"><img src="src/assets/tech_stack/reactjs.svg" alt=""></img>React</a>
-                    <a href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>Astro</a>
+                    <a id="lang" href="https://www.javascript.com/" target="_blank"><img src="src/assets/tech_stack/js.svg" alt="javascript"></img>JavaScript</a>
+                    <a id="lang" href="https://react.dev/" target="_blank"><img src="src/assets/tech_stack/reactjs.svg" alt=""></img>React</a>
+                    <a id="lang" href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>Astro</a>
                 </div>
                 <div className="quantum">Quantum
-                    <a href="https://www.python.org/" target="_blank"><img src="src/assets/tech_stack/python.svg" alt=""></img>Python</a>
-                    <a href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>Cirq</a>
+                    <a id="lang" href="https://www.python.org/" target="_blank"><img src="src/assets/tech_stack/python.svg" alt=""></img>Python</a>
+                    <a id="lang" href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>Cirq</a>
                 </div>
             </div>
             <div class="heading">In School</div>
             <div className="about">
-                <a href="https://dotnet.microsoft.com/en-us/languages/csharp" target="_blank"><img src="src/assets/tech_stack/csharp.svg" alt="C#"></img>C#</a>
-                <a href="https://go.dev/" target="_blank"><img src="src/assets/tech_stack/go.svg" alt="GO"></img><i>GO</i></a>
-                <a href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>F#</a>
-                <a href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>Java</a>
-                <a href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>Arduino</a>
+                <a id="lang" href="https://dotnet.microsoft.com/en-us/languages/csharp" target="_blank"><img src="src/assets/tech_stack/csharp.svg" alt="C#"></img>C#</a>
+                <a id="lang" href="https://go.dev/" target="_blank"><img src="src/assets/tech_stack/go.svg" alt="GO"></img><i>GO</i></a>
+                <a id="lang" href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>F#</a>
+                <a id="lang" href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>Java</a>
+                <a id="lang" href="" target="_blank"><img src="src/assets/tech_stack/.svg" alt=""></img>Arduino</a>
             </div>
         </div>
         </>

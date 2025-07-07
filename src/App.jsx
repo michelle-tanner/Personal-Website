47f85da75e49d7f9c95a925cfd810e4b9e2b5ec8
michelle <michellemt0518@gmail.com>
2025-07-07 09:42:49 -0500

    clean app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -74,27 +74,7 @@ function App() {
       </main>
 
     </div>
-
-
-
-
-      {/* <Navigation text="Test1" text2="Test1"/>
-      <Navigation />
-      <br></br><br></br> */}
-
-      {/* {eventNumber === 1 ? (
-        <EventCard event={{title: "St. Patrick's Day", url: "", date: "3/17/2024", description: "This was the day..."}}/>
-      ) : (
-      <EventCard event={{title: "Birthday Celebration", url: "", date: "3/14/2024", description: "First day meeting.."}}/>
-      )}
-
-      {eventNumber === 1 && <EventCard event={{title: "Birthday Celebration", url: "", date: "3/14/2024", description: "First day meeting.."}}/>}
-      {eventNumber === 2 && <EventCard event={{title: "St. Patrick's Day", url: "", date: "3/17/2024", description: "This was the day..."}}/>} */}
-      
-      
-      </>
-    
-    
+    </>
   )
 }
 
